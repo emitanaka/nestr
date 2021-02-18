45458b680f7d85a905a3ac3e6ae4d5b4710b7a27
Emi Tanaka <dr.emi.tanaka@gmail.com>
2021-02-18 19:54:37 +1100

    options

---
+++ R/options.R
@@ -0,0 +1,15 @@
+op.nestr <- list(
+  nestr.leading_zero = FALSE,
+  nestr.min_ndigits = 0
+)
+
+nestr_opt <- function(x, prefix = "nestr.") {
+  if(missing(x)) {
+    op.nestr
+  } else {
+    opt_name <- paste0(prefix, x)
+    res <- getOption(opt_name)
+    if(!is_null(res)) return(res)
+    op.nestr[[opt_name]]
+  }
+}
