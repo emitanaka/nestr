6d4965aa6e7678bbbdbba62ef6034ea426993422
Emi Tanaka <dr.emi.tanaka@gmail.com>
2021-02-19 18:42:38 +1100

    basic amplify test

--- tests/testthat/test-amplify.R
+++ tests/testthat/test-amplify.R
@@ -1,4 +1,11 @@
 test_that("amplify", {
-  data.frame(x = 1:2) %>%
-    amplify(y = nest_in(x, 3))
+
+  expect_equal(amplify(data.frame(x = 1:2) , y = nest_in(x, 3)),
+               data.frame(x = rep(1:2, each = 3),
+                          y = as.character(rep(1:3, times = 2))))
+
+  expect_equal(amplify(data.frame(x = c(1, 1, 2)) , y = nest_in(x, 3)),
+               data.frame(x = rep(c(1, 1, 2), each = 3),
+                          y = as.character(rep(1:3, times = 3))))
+
 })
