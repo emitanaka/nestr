1c925cd9de3a0bf94449dbe6cca262f7ca03ced3
Emi Tanaka <dr.emi.tanaka@gmail.com>
2021-02-19 18:34:10 +1100

    amplify

--- R/amplify.R
+++ R/amplify.R
@@ -40,10 +40,10 @@ amplify_cols <- function(.data, ...) {
 
   for(i in seq_along(dots)) {
     res <- eval_tidy(dots[[i]], new_data)
-    # TODO
-
     not_named <- (is.null(dots_names) || dots_names[i] == "")
     name <- if (not_named) auto_named_dots[i] else dots_names[i]
+    names(res)[2] <- name
+    new_data <- merge(new_data, res, by = names(res)[1])
   }
   new_data
 }
