cffcbf07674eaf24b173bc7b44c7785f8974cfca
Emi Tanaka <dr.emi.tanaka@gmail.com>
2021-02-20 13:22:20 +1100

    compact list class

---
+++ R/clist.R
@@ -0,0 +1,52 @@
+#' @export
+format.clist <- function(x, ..., dict = FALSE) {
+  class(x) <- setdiff(class(x), "clist")
+  if(dict) {
+    x
+  } else {
+    order <- attr(x, "order")
+    x[order]
+  }
+}
+
+#' @export
+print.clist <- function(x, ...) {
+  print(format(x, ...))
+}
+
+#' @export
+`[.clist` <- function(x, i, ...) {
+  iorder <- attr(x, "order")[i]
+  format(x, dict = TRUE)[iorder]
+}
+
+#' @export
+`[[.clist` <- function(x, i, ...) {
+  iorder <- attr(x, "order")[i]
+  format(x, dict = TRUE)[[iorder]]
+}
+
+
+#' @export
+`[<-.clist` <- function(x, i, ..., value) {
+  res <- format(x)
+  res[i] <- value
+  res
+}
+
+#' @export
+`[[<-.clist` <- function(x, i, ..., value) {
+  res <- format(x)
+  res[[i]] <- value
+  res
+}
+
+#' @export
+as.list.clist <- function(x, ...) {
+  format(x)
+}
+
+#' @export
+length.clist <- function(x, ...) {
+  length(attr(x, "order"))
+}
