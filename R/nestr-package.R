d0aabc7f4fe80e9ab5b8bc09bb91d21b6df2cf56
Emi Tanaka <dr.emi.tanaka@gmail.com>
2021-03-13 20:47:39 +1100

    doc update

--- R/nestr-package.R
+++ R/nestr-package.R
@@ -1,5 +1,7 @@
 #' @keywords internal
 #' @importFrom rlang enquos quo_is_null enquo eval_tidy f_lhs f_rhs as_string enexpr quo_get_expr is_symbol is_null %||%
+#' @importFrom rlang abort expr caller_env list2 f_env
+#' @importFrom vctrs vec_unique
 "_PACKAGE"
 
 # The following block is used by usethis to automatically manage
