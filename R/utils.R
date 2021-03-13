1b5056668b2e918f273d21c8cd28d7edad4a2a91
Emi Tanaka <dr.emi.tanaka@gmail.com>
2021-03-13 20:47:26 +1100

    move to utils

--- R/utils.R
+++ R/utils.R
@@ -2,5 +2,14 @@
 
 ndigits <- function(x, min_ndigits = NULL) {
   max(c(floor(log10(abs(x))) + 1,
-        max(min_ndigits, nestr_opt("min_ndigits"), 1)))
+        max(min_ndigits, as.numeric(nestr_opt("nestr.leading_zero")), 1)))
+}
+
+make_labels <- function(n, leading0, prefix, suffix) {
+  if(!isFALSE(leading0)) {
+    min_ndigits <- as.integer(leading0)
+    sprintf(paste0("%s%.", ndigits(n, min_ndigits), "d%s"), prefix, 1:n, suffix)
+  } else {
+    paste0(prefix, 1:n, suffix)
+  }
 }
